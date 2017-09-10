d353bda81791ddb3bfa69ce51be64fddbb29829a
Vincent B <evilznet@gmail.com>
2017-09-10 17:56:02 +0200

    002 - Basic routing

--- App.fs
+++ App.fs
@@ -1,4 +1,21 @@
 ﻿open Suave                 // always open suave
 open Suave.Successful      // for OK-result
+open Suave.Filters
+open Suave.Operators
+open Suave.RequestErrors
 
-startWebServer defaultConfig (OK "Hello World!")
+let browse =
+    request (fun r ->
+        match r.queryParam "genre" with
+        | Choice1Of2 genre -> OK (sprintf "Genre: %s" genre)
+        | Choice2Of2 msg -> BAD_REQUEST msg)
+        
+let webPart = 
+    choose [
+        path "/" >=> (OK "Home")
+        path "/store" >=> (OK "Store")
+        path "/store/browse" >=> browse
+        pathScan "/store/details/%d" (fun id -> OK (sprintf "Details %d" id))
+    ]
+
+startWebServer defaultConfig webPart
